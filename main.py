9213e05dbfa01706290b52bd38787093ae5a3b7e
tendersanic <161989346+tendersanic@users.noreply.github.com>
2024-05-18 00:54:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,5 @@ async def redirectProxy(url:str=None):
         async with httpx.AsyncClient() as client:
             response = await client.get(url, headers={'User-Agent':ua.random.strip()},follow_redirects=False)
             redirect = response.headers.get("Location") if response.headers else ""
+            print(response.text)
             return Response(content=redirect, media_type="text/html")
