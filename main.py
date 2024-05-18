3ba67b76aebfb8f480ebcbdd89f146f4f881c760
tendersanic <161989346+tendersanic@users.noreply.github.com>
2024-05-18 07:11:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ async def redirect_proxy(url:str=None):  # Corrected function name to follow nam
     else:
         ua = UserAgent()
         url = urllib.parse.unquote(url)
-        proxies_response = httpx.get("https://proxylist.geonode.com/api/proxy-list?limit=100&page=1&sort_by=speed&sort_type=asc")  # Changed variable name to avoid overwriting 'proxies'
-        proxies_data = proxies_response.json()  # Extract JSON data from response
-        socks4_proxies = [entry for entry in proxies_data["data"] if 'socks5' in entry.get('protocols', [])]
+        # proxies_response = httpx.get("https://proxylist.geonode.com/api/proxy-list?limit=100&page=1&sort_by=speed&sort_type=asc")  # Changed variable name to avoid overwriting 'proxies'
+        # proxies_data = proxies_response.json()  # Extract JSON data from response
+        # socks4_proxies = [entry for entry in proxies_data["data"] if 'socks5' in entry.get('protocols', [])]
 
-        random_socks4_proxy = random.choice(socks4_proxies)
-        socks4_ip = random_socks4_proxy['ip']
-        socks4_port = random_socks4_proxy['port']
-        proxy = f"socks5://{socks4_ip}:{socks4_port}"
-        async with httpx.AsyncClient(proxy=proxy) as client:  # Changed variable name to avoid overwriting 'proxies'
-            response = await client.get(url, headers={'User-Agent': ua.random.strip()}, follow_redirects=False)  # Changed variable name to avoid overwriting 'response'
+        # random_socks4_proxy = random.choice(socks4_proxies)
+        # socks4_ip = random_socks4_proxy['ip']
+        # socks4_port = random_socks4_proxy['port']
+        # proxy = f"socks5://{socks4_ip}:{socks4_port}"
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers={'User-Agent': ua.random.strip()}, follow_redirects=False) 
             redirect = response.headers.get("Location") if response.headers else ""
             print(response.text)
             return Response(content=redirect, media_type="text/html")
